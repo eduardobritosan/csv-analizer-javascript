5c790781adadd6591fbe88108a69ba759bdad916
hecrp <alu0100774429@ull.edu.es>
2015-03-11 19:29:02 +0000

    Modicados HTML, JS y CSS

--- csv.js
+++ csv.js
@@ -14,20 +14,18 @@ function calculate() {
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   var lines = temp.split(/\n+\s*/);
   var commonLength = NaN;
-  var r = [];
-  // Template using underscore
-  var row = "<% _.each(items, function(name) { %>"     +
-            "                    <td><%= name %></td>" +
-            "              <% }); %>";
+  var tableTemplate = document.getElementById("tableTemplate").innerHTML;
+  var row;
+  var rows = [];
 
   if (window.localStorage) localStorage.original  = temp;
-  
+
   for(var t in lines) {
     var temp = lines[t];
     var m = temp.match(regexp);
     var result = [];
     var error = false;
-    
+
     if (m) {
       if (commonLength && (commonLength != m.length)) {
         //alert('ERROR! row <'+temp+'> has '+m.length+' items!');
@@ -44,18 +42,18 @@ function calculate() {
         var removeescapedquotes = removelastquote.replace(/\\"/,'"');
         result.push(removeescapedquotes);
       }
-      var tr = error? '<tr class="error">' : '<tr>';
-      r.push(tr+_.template(row, {items : result})+"</tr>");
+      var tr = error ? 'error' : 'legal';
+      row = new Object();
+      row.type = tr;
+      row.items = result;
+      rows.push(row);
     }
     else {
-      alert('ERROR! row '+temp+' does not look as legal CSV');
+      alert('ERROR! row '+temp +' does not look as legal CSV');
       error = true;
     }
   }
-  r.unshift('<p>\n<table class="center" id="result">');
-  r.push('</table>');
-  //alert(r.join('\n')); // debug
-  finaltable.innerHTML = r.join('\n');
+  finaltable.innerHTML = _.template(tableTemplate, { rows: rows });
 }
 
 window.onload = function() {
