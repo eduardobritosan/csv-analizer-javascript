3de9a15ff5ea1d135b450487b665299fbb2ab970
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-03-11 20:10:09 +0000

    Agregados tests, mocha, chai y blanket

---
+++ tests/test.js
@@ -0,0 +1,29 @@
+var assert = chai.assert;
+//var converted;
+//var original;
+
+suite('csv', function() {
+	if (typeof __html__ !== 'undefined') {
+              document.body.innerHTML = __html__['tests/index.html'];
+              original = document.getElementById('original');
+              finaltable = document.getElementById('finaltable');
+          }   
+	test('2, 4, "hola"', function() {
+		original.value = '2, 4, "hola"';
+		calculate();
+		assert.deepEqual(finaltable.children[0].children[0].children[0].children[0].children[0].childNodes[0].data, "2");	});
+	
+	test('2, 4, "hola"', function() {
+	original.value = '2, 4, "hola"';
+		calculate();
+		assert.deepEqual(finaltable.children[0].children[0].children[0].children[0].children[1].childNodes[0].data, " 4");
+	});
+
+	test('2, 4, "hola"', function() {
+		original.value = '2, 4, "hola"';
+		calculate();
+		assert.deepEqual(finaltable.children[0].children[0].children[0].children[0].children[2].childNodes[0].data, "hola");
+	});
+	
+	
+});
