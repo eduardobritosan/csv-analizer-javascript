01cf89777ad16fefbe2e2a0444ea3c4a3f76ff98
hecrp <alu0100774429@ull.edu.es>
2015-03-18 18:16:10 +0000

    Acabados ficheros de app con express y csv

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+var _ = require('underscore');
+var $ = require('jquery');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (request, response) {
+    response.render('index', { title: 'CSV Analyzer' });
+});
+
+app.get('/separateCSV', function (request, response) {
+
+    var result;
+    var original = request.query.input
+    var temp = original;
+    var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
+    var lines = temp.split(/\n+\s*/);
+    var commonLength = NaN;
+    var row;
+    var rows = [];
+
+    for (var t in lines) {
+        var temp = lines[t];
+        var m = temp.match(regexp);
+        var result = [];
+        var error = false;
+
+        if (m) {
+            if (commonLength && (commonLength != m.length)) {
+                //alert('ERROR! row <'+temp+'> has '+m.length+' items!');
+                error = true;
+            }
+            else {
+                commonLength = m.length;
+                error = false;
+            }
+            for (var i in m) {
+                var removecomma = m[i].replace(/,\s*$/, '');
+                var remove1stquote = removecomma.replace(/^\s*"/, '');
+                var removelastquote = remove1stquote.replace(/"\s*$/, '');
+                var removeescapedquotes = removelastquote.replace(/\\"/, '"');
+                result.push(removeescapedquotes);
+            }
+            var tr = error ? 'error' : 'legal';
+            row = new Object();
+            row.type = tr;
+            row.items = result;
+            rows.push(row);
+        }
+        else {
+            alert('ERROR! row ' + temp + ' does not look as legal CSV');
+            error = true;
+        }
+    }
+    response.send({ "rows": rows });
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
--- csv.js
+++ csv.js
@@ -2,59 +2,16 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 $(document).ready(function() {
-   $("button").click(function() {
-     calculate();
+    $("button").click(function () {
+        if (window.localStorage) localStorage.original = document.getElementById("original").value;
+        console.log(document.getElementById("original").value);
+        $.get("/separateCSV", { input: document.getElementById("original").value }, function (data) {
+            var tableTemplate = document.getElementById("tableTemplate").innerHTML;
+            $("#finaltable").html(_.template(tableTemplate, { rows: data.rows }));
+           console.log(_.template(tableTemplate, { rows: data.rows }));
+       }, 'json');
    });
  });
-
-function calculate() {
-  var result;
-  var original       = document.getElementById("original");
-  var temp = original.value;
-  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
-  var lines = temp.split(/\n+\s*/);
-  var commonLength = NaN;
-  var tableTemplate = document.getElementById("tableTemplate").innerHTML;
-  var row;
-  var rows = [];
-
-  if (window.localStorage) localStorage.original  = temp;
-
-  for(var t in lines) {
-    var temp = lines[t];
-    var m = temp.match(regexp);
-    var result = [];
-    var error = false;
-
-    if (m) {
-      if (commonLength && (commonLength != m.length)) {
-        //alert('ERROR! row <'+temp+'> has '+m.length+' items!');
-        error = true;
-      }
-      else {
-        commonLength = m.length;
-        error = false;
-      }
-      for(var i in m) {
-        var removecomma = m[i].replace(/,\s*$/,'');
-        var remove1stquote = removecomma.replace(/^\s*"/,'');
-        var removelastquote = remove1stquote.replace(/"\s*$/,'');
-        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
-        result.push(removeescapedquotes);
-      }
-      var tr = error ? 'error' : 'legal';
-      row = new Object();
-      row.type = tr;
-      row.items = result;
-      rows.push(row);
-    }
-    else {
-      alert('ERROR! row '+temp +' does not look as legal CSV');
-      error = true;
-    }
-  }
-  finaltable.innerHTML = _.template(tableTemplate, { rows: rows });
-}
 
 window.onload = function() {
   // If the browser supports localStorage and we have some stored data
